b884a1ea11c2bcd395bcdd5fc6d017a54634a4f7
nusubbotin <nu.subbotin@ya.ru>
2022-09-10 12:53:33 +0300

    Реализация требований по заданию №2

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,43 @@ public class Main {
         System.out.println("Значение переменной varExampleFloat с типом float равно " + varExampleFloat);
         System.out.println("Значение переменной varExampleDouble с типом double равно " + varExampleDouble);
 
+        System.out.println("task2:");
+        float floatNumber = 27.12f;
+        long longNumber = 987678965549L;
+        float floatExample2 = 2.786f;
+        boolean boolExample = false;
+        short shortNumber = 569;
+        short shortExample2 = -159;
+        short shortExample3 = 27897;
+        float floatExample3 = 67.0f;
+
+        //тестовый вывод
+        System.out.println("floatNumber="+floatNumber);
+        System.out.println("longNumber="+longNumber);
+        System.out.println("floatExample2="+floatExample2);
+        System.out.println("boolExample="+boolExample);
+        System.out.println("shortNumber="+shortNumber);
+        System.out.println("shortExample2="+shortExample2);
+        System.out.println("shortExample3="+shortExample3);
+        System.out.println("floatExample3="+floatExample3);
+
+        int deskCnt = 1;
+        byte productType = 5;
+        short serialNumber = 31232;
+        long rowId = 2321321332423423L;
+        float price = 150000.50f;
+        double balance = 16600000000.550;
+        boolean flPutReport = true;
+        char symbol$ = 36;
+
+        //тестовый вывод
+        System.out.println("deskCnt="+deskCnt);
+        System.out.println("productType="+productType);
+        System.out.println("serialNumber="+serialNumber);
+        System.out.println("rowID="+rowId);
+        System.out.println("price="+price);
+        System.out.println("balance="+balance);
+        System.out.println("flPutReport="+flPutReport);
+        System.out.println("symbol$="+symbol$);
     }
 }
