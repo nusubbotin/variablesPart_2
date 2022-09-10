30fe3d947d9595923e47d2e7c62d2a4654ee2368
nusubbotin <nu.subbotin@ya.ru>
2022-09-10 12:52:41 +0300

    Реализация требований по заданию №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("task1:");
+        int varExampleInt = 777;
+        byte varExampleByte = 55;
+        short varExampleShort = 3555;
+        long varExampleLong = 1234567890;
+        float varExampleFloat = 32312.31f;
+        double varExampleDouble = 950.55555;
+
+        System.out.println("Значение переменной varExampleInt с типом int равно " + varExampleInt);
+        System.out.println("Значение переменной varExampleByte с типом byte равно " + varExampleByte);
+        System.out.println("Значение переменной varExampleShort с типом short равно " + varExampleShort);
+        System.out.println("Значение переменной varExampleLong с типом long равно " + varExampleLong);
+        System.out.println("Значение переменной varExampleFloat с типом float равно " + varExampleFloat);
+        System.out.println("Значение переменной varExampleDouble с типом double равно " + varExampleDouble);
+
     }
 }
